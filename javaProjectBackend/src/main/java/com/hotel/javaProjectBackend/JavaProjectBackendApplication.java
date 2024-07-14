535c2a6fd36991f25ac34361420d5b55770d4dd8
Gaurav Singh <gauravsingh.mkp2002@gmail.com>
2024-07-14 23:50:25 +0530

    initial commit

---
+++ javaProjectBackend/src/main/java/com/hotel/javaProjectBackend/JavaProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.javaProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectBackendApplication.class, args);
+	}
+
+}
---
+++ javaProjectBackend/src/test/java/com/hotel/javaProjectBackend/JavaProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.javaProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
